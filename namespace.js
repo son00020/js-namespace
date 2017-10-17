af4e0b06229b158778282d5af1077711b6f9301f
kezbo28 <32390723+kezbo28@users.noreply.github.com>
2017-10-16 21:52:31 -0400

    upload assigment

---
+++ namespace.js
@@ -0,0 +1,26 @@
+var SON00020 = {
+    
+  init: function() {
+   var div = document.createElement("div");
+      div.className = "box";
+      div.textContent = "SON00020";
+      document.getElementById("boxes").appendChild(div);
+      
+      div.addEventListene("click",clickNow);
+      div.addEventListener("mouseover", hoverMouse);
+      div.addEventListener("mouseout", hoverMouse);
+      
+      function clickNow(ev) {
+          ev.currentTarget.backgroundColor = "red";
+          ev.currentTarget.borderColor = "yellow";
+             
+      }
+       
+     function hoverMouse(ev)  {
+         ev.classList.toggle("highlight");
+         
+     }
+      
+  }
+    
+}
